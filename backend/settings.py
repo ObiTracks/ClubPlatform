e47846f41a2a53b7b8fede81498502edb0bffd14
Obi Ihejirika <oihejirika@Obis-MacBook-Air.local>
2021-10-10 15:15:56 -0400

    Added environment variables

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
+
+# Environment variables configuration. The how to link is here:
+# https://alicecampkin.medium.com/how-to-set-up-environment-variables-in-django-f3c4db78c55f
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY', '14c9mfbl*+fxx9csz=a)g_7q_@2)+kw$!k5uv$7zuvk(*mip+a')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DEBUG')
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
