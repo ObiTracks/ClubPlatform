aae9ccc5a81c4e30dc7e8f7aa304b123bcd11160
ObiTracks <straightup.stack@gmail.com>
2022-02-14 00:07:43 -0500

    updates to views comment

--- mainplatform/views.py
+++ mainplatform/views.py
@@ -1,14 +1,21 @@
+# Python packages
+from datetime import date
 from django.contrib.admin.views.decorators import staff_member_required
+
+#Imports for Django views
 from django.http import Http404
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+
+#Imports for django forms
 from django.forms import inlineformset_factory
-from datetime import date
-from django.core.paginator import Paginator
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
+
+#Django authentication and messaging features
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.paginator import Paginator
 
 from .forms import *
 from .models import *
